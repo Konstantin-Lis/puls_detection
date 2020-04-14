3fb00aa8c3235143655fdab58bd16a07a4e47ea6
Konstantin-Lis <55928181+Konstantin-Lis@users.noreply.github.com>
2020-04-14 23:10:44 +0500

    Update video_maker.py

--- video_maker.py
+++ video_maker.py
@@ -1,26 +1,38 @@
 import pygame
 import os
+import json
+
+adr = "C://USERS/USER/desktop/01-01.json"  #  адрес файла json
+with open (adr) as file:
+    y = json.load(file)
 
 adress = "C://USERS/USER/desktop/01-01/"  # адрес папки, в которой хранятся фото
-
 photo_list = os.listdir(adress)  # список, где находятся все файлы
-print(photo_list)
 
 pygame.init()
-sc = pygame.display.set_mode((640, 480)) # размеры окна вывода
+sc = pygame.display.set_mode((640, 520)) # размеры окна вывода
 
 clock = pygame.time.Clock()
-FPS = 2 #тут находится FPS
+FPS = 24 #тут находится FPS
 j = 0
 z = 0
+
+fon_surf = pygame.image.load("black_fon.png")
+fon_rect = fon_surf.get_rect()
+
+text_format = pygame.font.SysFont("Arial", 20) # шрифт и размер текста
 
 play = True
 while play == True:  # основной цикл программы
     if z%2 == 1:
+        sc.blit(fon_surf, fon_rect) # крч, я еще добавил фон, чтобы текст не накладывался, у меня это черная картинка
+        # и я ее закинул в папку с программой для сборки фото в видео
         new_ad = adress + photo_list[j]
         img_surf = pygame.image.load(new_ad)
         img_rect = img_surf.get_rect()
         sc.blit(img_surf, img_rect)
+        text = text_format.render(str(y["/FullPackage"][j]['Value']['pulseRate']), 0, (255,0,0))
+        sc.blit(text, (310,490))
         j += 1
 
     clock.tick(FPS)
