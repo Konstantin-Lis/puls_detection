e96a68fba17aea9fc43404cc2a28c8e95da72b94
Konstantin-Lis <55928181+Konstantin-Lis@users.noreply.github.com>
2020-04-28 19:33:20 +0500

    Update plot_maker.py

--- plot_maker.py
+++ plot_maker.py
@@ -5,19 +5,23 @@ adr_1 = "C://USERS/USER/desktop/01-01.json"  #  адрес файла json
 with open (adr_1) as file_1:
     y1 = json.load(file_1)
 lst_y1 = []
-lst_x = []
+lst_x1 = []
 for j in range(len(y1["/FullPackage"])):
     if j < 24*90+1:
         lst_y1.append(y1["/FullPackage"][j]['Value']['pulseRate'])
         lst_x.append(j/24)
 
 lst_y2 = []
+lst_x2 = []
+i = 0
 adr_2 = "C://USERS/USER/desktop/01-01.txt" #  адрес блокнотовского файла
 file_2 = open(adr_2, 'r')
 for line in file_2:
     lst_y2.append(int(line))
+    lst_x2.append(i)
+    i += 1
 
 plt.grid()
-plt.plot(lst_x, lst_y1)
-plt.plot(lst_x, lst_y2, color = "red")
+plt.plot(lst_x1, lst_y1)
+plt.plot(lst_x2, lst_y2, color = "red")
 plt.show()
